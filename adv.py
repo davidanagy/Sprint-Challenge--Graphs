494f25e3049c3ce57b3c80553eacced7d8a1dcf3
davidanagy <davidanagy@users.noreply.github.com>
2020-04-16 14:06:03 -0700

    successfully made a traversal path with 1007 rooms

--- adv.py
+++ adv.py
@@ -27,8 +27,110 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+def add_current_room_to_room_dict(player, room_dict):
+    room = player.current_room
+    room_id = room.id
+    room_dict[room_id] = {'exits': {}}
+    room_info = room_dict[room_id]
+    exits = room.get_exits()
+    for d in exits:
+        room_info['exits'][d] = '?'
+    if len(room_info['exits']) == 1:
+        room_info['fully_explored'] = True
+    else:
+        room_info['fully_explored'] = False
+
+
+def check_if_fully_explored(room_dict, room_id):
+    exits = room_dict[room_id]['exits']
+    fully_explored = True
+    for d in exits:
+        if exits[d] == '?':
+            fully_explored = False
+            break
+    room_dict[room_id]['fully_explored'] = fully_explored
+
+
+def travel_and_log(player, direction, traversal_path, room_dict, rooms_explored):
+    current_room_id = player.current_room.id
+    print(f'Traveling {direction} from room {current_room_id}')
+    traversal_path.append(direction)
+    player.travel(direction)
+    next_room_id = player.current_room.id
+    room_dict[current_room_id]['exits'][direction] = next_room_id
+    check_if_fully_explored(room_dict, current_room_id)
+    if next_room_id not in room_dict:
+        add_current_room_to_room_dict(player, room_dict)
+        rooms_explored += 1
+    direction_pairs = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+    opp_dir = direction_pairs[direction]
+    room_dict[next_room_id]['exits'][opp_dir] = current_room_id
+    check_if_fully_explored(room_dict, next_room_id)
+    return rooms_explored
+
+
+def bfs_for_unexplored_room(player, room_dict):
+    room_id = player.current_room.id
+    searched_rooms = {room_id}
+    queue = []
+    current_path = [room_id]
+    while True:
+        current_room_id = current_path[-1]
+        exits = room_dict[current_room_id]['exits']
+        for d in exits:
+            room_in_d_id = exits[d]
+            if room_in_d_id not in searched_rooms:
+                new_path = current_path.copy()
+                new_path.append(room_in_d_id)
+                if not room_dict[room_in_d_id]['fully_explored']:
+                    return new_path
+                else:
+                    searched_rooms.add(room_in_d_id)
+                    queue.append(new_path)
+        current_path = queue.pop(0)
+
+
 traversal_path = []
 
+room_dict = {}
+
+
+start_id = player.current_room.id
+start_exits = player.current_room.get_exits()
+room_dict[start_id] = {'exits': {}, 'fully_explored': False}
+for d in start_exits:
+    room_dict[start_id]['exits'][d] = '?'
+rooms_explored = 1
+target_rooms = len(room_graph)
+
+while True:
+    room = player.current_room
+    room_id = room.id
+    exits = room_dict[room_id]['exits']
+    for d in exits:
+        if exits[d] == '?':
+            next_dir = d
+            break
+    rooms_explored = travel_and_log(player, next_dir, traversal_path, room_dict, rooms_explored)
+    if rooms_explored == target_rooms:
+        break
+
+    next_room = player.current_room
+    next_room_id = next_room.id
+    next_room_info = room_dict[next_room_id]
+    if next_room_info['fully_explored']:
+        final_path = bfs_for_unexplored_room(player, room_dict)
+        for i in range(len(final_path)-1):
+            current_room_id = final_path[i]
+            next_room_id = final_path[i+1]
+            exits = room_dict[current_room_id]['exits']
+            for d in exits:
+                if exits[d] == next_room_id:
+                    next_d = d
+                    break
+            rooms_explored = travel_and_log(player, next_d, traversal_path, room_dict, rooms_explored)
+            if rooms_explored == target_rooms:
+                break
 
 
 # TRAVERSAL TEST
@@ -51,12 +153,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
